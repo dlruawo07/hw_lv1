c944da1b3a5a5b55bd6c7c981e00c044a050f2df
dlruawo07 <dlruawo07@gmail.com>
2023-04-18 00:41:57 +0900

    refactoring

--- schemas/index.js
+++ schemas/index.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
+    // .connect("mongodb://3.38.167.244:27017/hw_lv1")
     .connect("mongodb://localhost:27017/hw_lv1")
+    .then(() => {
+      console.log("Successfully connected to Mongo");
+    })
     .catch((err) => console.log(err));
 };
 
 mongoose.connection.on("error", (err) => {
-  console.error("몽고디비 연결 에러", err);
+  console.error("Failed to connect to Mongo", err);
 });
 
 module.exports = connect;
